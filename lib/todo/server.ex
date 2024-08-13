b0fab9ef16a404e0b364ddc6d589348109da64f5
ben lim <lim6112j@gmail.com>
2024-08-14 00:24:41 +0900

    back to genserver from agent, with timeout

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,25 +1,22 @@
 defmodule Todo.Server do
-	use Agent, restart: :temporary
+	use GenServer, restart: :temporary
+	@expiry_idle_timeout :timer.seconds(10)
 	def start_link(server_name) do
-		Agent.start_link(fn ->
-			IO.puts "Starting to-dp sever for #{server_name}"
-			{server_name, Todo.Database.get(server_name) || Todo.List.new}
-		end, name: via_tuple(server_name))
+		IO.puts "starting todo server for #{server_name}"
+		GenServer.start_link(__MODULE__,server_name, name: via_tuple(server_name))
 	end
 
+	@impl true
+	def init(server_name) do
+		{:ok, {server_name, nil}, {:continue, :init}}
+	end
 	def add_entry(todo_server, new_entry) do
 		IO.puts "Todo.Server.add_entry argument, new_entry : #{inspect(new_entry)}"
-		Agent.cast(todo_server, fn {name, todo_list} ->
-			new_state = Todo.List.add_entry(todo_list, new_entry)
-			Todo.Database.store(name, new_state)
-			{name, new_state}
-			end)
+		GenServer.cast(todo_server, {:add_entry, new_entry})
 	end
 
 	def entries(todo_server, date \\ nil) do
-		Agent.get(todo_server, fn {_name, todo_list}->
-			Todo.List.entries(todo_list, date)
-		end)
+		GenServer.call(todo_server, {:entries, date})
 	end
 	def via_tuple(name) do
 		{:via, Todo.ProcessRegistry, "todo server register", "possible?"}
@@ -28,4 +25,40 @@ defmodule Todo.Server do
 	def whereis(name) do
 		Todo.ProcessRegistry.whereis_name(name)
 	end
+
+	@impl true
+	def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
+		IO.puts "Todo.Server handle_cast: add_entry , todo_list = #{inspect(todo_list)}"
+		new_state = Todo.List.add_entry(todo_list, new_entry)
+		Todo.Database.store(name, new_state)
+		{:noreply, {name, new_state}, @expiry_idle_timeout}
+	end
+
+	@impl true
+	def handle_call({:entries, date}, _, {name, todo_list}) do
+		IO.puts "current todo_list : #{inspect(todo_list)}"
+		{
+			:reply,
+			Todo.List.entries(todo_list, date),
+			{name, todo_list},
+			@expiry_idle_timeout
+		}
+	end
+
+	@impl true
+	def handle_info(:read_init, state) do
+		{:noreply, {state, Todo.Database.get(state) || Todo.List.new}}
+	end
+	def handle_info(:timeout, {name, todo_list}) do
+		IO.puts "idle expire timeout, processs exiting: #{name}"
+		{:stop, :normal, {name, todo_list}}
+	end
+
+	@impl true
+	def handle_continue(:init, {name, nil}) do
+		todo_list = Todo.Database.get(name) || Todo.List.new
+		{:noreply, {name, todo_list}, @expiry_idle_timeout}
+	end
+
+
 end
