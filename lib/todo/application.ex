9163500f31cfaef14868aba9744a9b0d52da9573
ben lim <lim6112j@gmail.com>
2024-08-14 14:54:28 +0900

    application added

---
+++ lib/todo/application.ex
@@ -0,0 +1,6 @@
+defmodule Todo.Application do
+	use Application
+	def start(_,_) do
+		Todo.Supervisor.start_link()
+	end
+end
--- test/todo_test.exs
+++ test/todo_test.exs
@@ -1,8 +1,6 @@
 defmodule TodoTest do
   use ExUnit.Case
-  doctest Todo
 
-  test "greets the world" do
-    assert Todo.hello() == :world
-  end
+
+
 end
